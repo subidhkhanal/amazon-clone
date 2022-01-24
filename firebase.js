2ae56357bb06e45bd2820418044f3010a6744bf7
Subidh Khanal <subidhkhanal37@gmail.com>
2022-01-24 21:03:33 +0545

    Add files via upload
    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCG8up3vI2W9fAYITDAOtUbC66fjlmEFaI",
+    authDomain: "fir-3b3b0.firebaseapp.com",
+    projectId: "fir-3b3b0",
+    storageBucket: "fir-3b3b0.appspot.com",
+    messagingSenderId: "700311928170",
+    appId: "1:700311928170:web:f7f0c9053efc7b6a533913"
+};
